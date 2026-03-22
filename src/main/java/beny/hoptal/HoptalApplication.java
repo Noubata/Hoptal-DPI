6f778af0249b80bcca106d45f26f4b8cda9d364b
Beny <benynouba@gmail.com>
2026-03-23 00:21:23 +0100

    Hoptal DPI

---
+++ src/main/java/beny/hoptal/HoptalApplication.java
@@ -0,0 +1,13 @@
+package beny.hoptal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoptalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoptalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/beny/hoptal/HoptalApplicationTests.java
@@ -0,0 +1,13 @@
+package beny.hoptal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoptalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ea6b2bd8e3a41e2d4b27f5eb3bcdf6ac155f6f28
Beny <benynouba@gmail.com>
2026-03-26 14:19:39 +0100

    Global exception handler

--- src/main/java/beny/hoptal/exceptions/GlobalExceptionHandler.java
+++ src/main/java/beny/hoptal/exceptions/GlobalExceptionHandler.java
@@ -1,7 +1,32 @@
 package beny.hoptal.exceptions;
 
+import beny.hoptal.dtos.responses.ActiverDesactiverUserResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(AncienMotDePasseIncorrectException.class)
+    public ResponseEntity<String> handleAncienMotDePasseException(Exception error) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+    @ExceptionHandler(MotDePasseIncorrectException.class)
+    public ResponseEntity<String> handleMotDePasseException(Exception error) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+    @ExceptionHandler(RoleIntrouvableException.class)
+    public ResponseEntity<String> handleRoleIntrouvableException(Exception error) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+    @ExceptionHandler(UtilisateurExisteDejaException.class)
+    public ResponseEntity<String> handleUtilisateurExisteDejaException(Exception error) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(error.getMessage());
+    }
+    @ExceptionHandler(UtilisateurIntrouvableEception.class)
+    public ResponseEntity<String> handleUtilisateurIntrouvableException(Exception error) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+
 }

a284c624028e047a6d0ac1200c81ff67eff843e0
Beny <benynouba@gmail.com>
2026-03-31 13:35:20 +0100

    properties

---
+++ src/main/java/beny/hoptal/data/repositories/RoleRepository.java
@@ -0,0 +1,11 @@
+package beny.hoptal.data.repositories;
+
+import beny.hoptal.data.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByNom(@Param("nom") String nom);
+}
---
+++ src/main/java/beny/hoptal/exceptions/RoleIntrouvableException.java
@@ -0,0 +1,7 @@
+package beny.hoptal.exceptions;
+
+public class RoleIntrouvableException extends RuntimeException {
+    public RoleIntrouvableException(String message) {
+        super(message);
+    }
+}

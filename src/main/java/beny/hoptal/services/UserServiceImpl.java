03ce6171e9192bbbbcec2c6cc461fc96b3084f42
Beny <benynouba@gmail.com>
2026-03-31 13:37:11 +0100

    implementation

---
+++ src/main/java/beny/hoptal/services/UserServiceImpl.java
@@ -0,0 +1,99 @@
+package beny.hoptal.services;
+
+import beny.hoptal.Security.JwtService;
+import beny.hoptal.data.models.Role;
+import beny.hoptal.data.models.User;
+import beny.hoptal.data.repositories.RoleRepository;
+import beny.hoptal.data.repositories.UserRepository;
+import beny.hoptal.dtos.requests.ActiverDesactiverUserRequest;
+import beny.hoptal.dtos.requests.ChangerMotDePasseRequest;
+import beny.hoptal.dtos.requests.CreerUserRequest;
+import beny.hoptal.dtos.requests.LoginRequest;
+import beny.hoptal.dtos.responses.ActiverDesactiverUserResponse;
+import beny.hoptal.dtos.responses.ChangerMotDePasseResponse;
+import beny.hoptal.dtos.responses.LoginResponse;
+import beny.hoptal.exceptions.*;
+import beny.hoptal.utils.UserMapper;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+public class UserServiceImpl implements UserService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RoleRepository roleRepository;
+    @Autowired
+    JwtService jwtService;
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+    @Transactional
+    @Override
+    public LoginResponse login(LoginRequest request) {
+        User user = userRepository.findByNomUtilisateur(request.getNomUtilisateur())
+                .orElseThrow(() -> new UtilisateurIntrouvableEception("Utilisateur introuvable."));
+
+        if (!user.getActif()) {
+            throw new RuntimeException("Ce compte est désactivé. Contactez l'administrateur.");
+        }
+
+        if (!passwordEncoder.matches(request.getMotDePasse(), user.getMotDePasseHashe())) {
+            throw new MotDePasseIncorrectException("Mot de passe incorrect.");
+        }
+        String token = jwtService.generateToken(user);
+        LoginResponse response = new LoginResponse();;
+        response.setToken(token);
+        response.setUserId(user.getId());
+        response.setNomUtilisateur(user.getNomUtilisateur());
+        response.setRole(user.getRole().getNom());
+
+        return response;
+    }
+
+    @Transactional
+    @Override
+    public User creerUser(CreerUserRequest request) {
+        if (userRepository.findByNomUtilisateur(request.getNomUtilisateur()).isPresent()) {
+            throw new UtilisateurExisteDejaException("Le nom d'utilisateur '"
+                    + request.getNomUtilisateur() + "' est déjà utilisé.");
+        }
+        Role role = roleRepository.findById(request.getRoleId())
+                .orElseThrow(() -> new RoleIntrouvableException("Rôle introuvable."));
+
+        User user = new User();
+        user.setNomUtilisateur(request.getNomUtilisateur());
+        user.setMotDePasseHashe(passwordEncoder.encode(request.getMotDePasse()));
+        user.setRole(role);
+        user.setActif(true);
+        user.setDateCreation(LocalDateTime.now());
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public ChangerMotDePasseResponse changerMotDePasse(Long userId, ChangerMotDePasseRequest request) {
+
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UtilisateurIntrouvableEception("Utilisateur introuvable."));
+
+        if (!passwordEncoder.matches(request.getAncienMotDePasse(), user.getMotDePasseHashe())) {
+            throw new AncienMotDePasseIncorrectException("Ancien mot de passe incorrect.");
+        }
+
+        user.setMotDePasseHashe(passwordEncoder.encode(request.getNouveauMotDePasse()));
+        userRepository.save(user);
+        return new ChangerMotDePasseResponse("Mot de passe modifié avec succès.");
+    }
+
+    @Transactional
+    public ActiverDesactiverUserResponse activerDesactiverUser(Long userId, ActiverDesactiverUserRequest request) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UtilisateurIntrouvableEception("Utilisateur introuvable."));
+        user.setActif(request.getActif());
+        userRepository.save(user);
+        return UserMapper.toActiverDesactiverUserResponse(user);
+    }
+}

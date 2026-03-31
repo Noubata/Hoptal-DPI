397102a01889c168312ea522a9b48d1fac932df6
Beny <benynouba@gmail.com>
2026-03-31 13:36:36 +0100

    exceptioons

---
+++ src/main/java/beny/hoptal/exceptions/UtilisateurExisteDejaException.java
@@ -0,0 +1,7 @@
+package beny.hoptal.exceptions;
+
+public class UtilisateurExisteDejaException extends RuntimeException {
+    public UtilisateurExisteDejaException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/beny/hoptal/exceptions/UtilisateurIntrouvableEception.java
@@ -0,0 +1,7 @@
+package beny.hoptal.exceptions;
+
+public class UtilisateurIntrouvableEception extends RuntimeException {
+    public UtilisateurIntrouvableEception(String message) {
+        super(message);
+    }
+}

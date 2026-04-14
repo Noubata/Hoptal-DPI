6f1a3eb1d62b7dcfeec3924b5b8fa17cae0add69
Beny <benynouba@gmail.com>
2026-04-14 19:09:31 +0100

    cors

--- src/main/java/beny/hoptal/Config/CorsConfig.java
+++ src/main/java/beny/hoptal/Config/CorsConfig.java
@@ -1,6 +1,5 @@
 package beny.hoptal.Config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;

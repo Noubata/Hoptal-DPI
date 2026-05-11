69943633d4b3426ae7ba53570486fc3f9fa1374c
Beny <benynouba@gmail.com>
2026-05-11 11:17:04 +0100

    retrouver hopital id;

--- src/main/java/beny/hoptal/data/repositories/ReleveMedicaleRepository.java
+++ src/main/java/beny/hoptal/data/repositories/ReleveMedicaleRepository.java
@@ -4,11 +4,13 @@ import beny.hoptal.data.models.ReleveMedicale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ReleveMedicaleRepository extends JpaRepository<ReleveMedicale, Long> {
     List<ReleveMedicale> findByPatientId(@Param("patientId") Long patientId);
 
-
+    Long countByDateDeVisiteBetween(LocalDateTime debut, LocalDateTime fin);
     List<ReleveMedicale> findByDoctorId(@Param("doctorId") Long doctorId);
 }

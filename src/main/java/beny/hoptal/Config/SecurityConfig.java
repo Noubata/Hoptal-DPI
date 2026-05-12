f492c3f3b3a3d3d97d498b235066b8ac66f671e1
Beny <benynouba@gmail.com>
2026-05-12 16:16:19 +0100

    frontend URL

--- src/main/java/beny/hoptal/Config/SecurityConfig.java
+++ src/main/java/beny/hoptal/Config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("http://localhost:5173",
-                "https://hoptal-frontend.vercel.app/"));
+                "https://hoptal-frontend.vercel.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
